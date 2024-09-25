1af15442bbe1afc42afe43b8828c38139752e384
Maurizio Bussi <maurizio.bussi.mb@gmail.com>
2024-09-25 15:27:53 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({
+        "version": "v0.0.1-alpha",
+        "response": {
+                "type": "Text",
+            }
+        "message": "test message",
+        "alive": True
+        }
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
